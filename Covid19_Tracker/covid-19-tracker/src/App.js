d7d991aeb92f7295133ac67f3d0bfb34203f711d
vikas <moganevikas@gmail.com>
2020-10-18 02:33:40 +0530

    Initial

---
+++ Covid19_Tracker/covid-19-tracker/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import {
+  MenuItem,FormControl,select,
+} from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+        <div className="App__header">
+
+      <h1>Covid 19 Tracker</h1>
+    <FormControl className="app__dropdown">
+
+  <select variant="outlined" value="abc">
+
+    <MenuItem value="worldwide">worldwide</MenuItem>
+    <MenuItem value="worldwide">dd</MenuItem>
+    <MenuItem value="worldwide">dfd</MenuItem>
+    <MenuItem value="worldwide">worldwide</MenuItem>
+    <MenuItem value="worldwide">dfd</MenuItem>
+
+
+  </select>
+</FormControl>
+</div>
+
+{/*Header*/}
+{/*Title - Select Input 
+dropdown field*/}
+
+{/*InfoBoxes*/}
+{/*InfoBoxex*/}
+{/*InfoBoxes*/}
+
+{/*Table*/}
+{/*Graph*/}
+
+
+{/*Map*/}
+
+
+    </div>
+  );
+}
+
+export default App;
