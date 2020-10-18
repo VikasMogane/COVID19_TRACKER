aa9d6f45fd667cc3939d5d87f54650bf854e5eed
vikas <moganevikas@gmail.com>
2020-10-18 13:22:19 +0530

    load dynamic country and countrycode from https://corona.lmao.ninja/docs api

--- Covid19_Tracker/covid-19-tracker/src/App.js
+++ Covid19_Tracker/covid-19-tracker/src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import './App.css';
-import {
-  MenuItem,FormControl,select,
-} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {  MenuItem,FormControl, Select } from "@material-ui/core";
 
 function App() {
+ 
+  const[countries, setCountries ] = useState([]);
+  const [country, setcountry]  = useState('worldWide');
+
+
+//https://disease.sh/v3/covid-19/countries
+
+// USEEFFECT =  RUN a piece of code     
+// based of given condition
+
+useEffect(() => {
+// then code inside here will run once 
+// when the component loads and not again
+
+//Async --> send a request 
+
+const getCountriesData = async() => {
+
+  await fetch("https://disease.sh/v3/covid-19/countries")
+  .then((response) => response.json())
+  .then((data) => {
+
+    const countries = data.map((country)=> ({
+      name:country.country, //India , United States
+      value: country.countryInfo.iso2,// IN, USA
+    }));
+    setCountries(countries);
+  });
+};
+getCountriesData();
+
+},[]);
+
+
+
+const onCountryChange =  async(event) =>{
+  const countryCode = event.target.value;
+  setcountry(countryCode);
+}
   return (
     <div className="App">
         <div className="App__header">
@@ -12,19 +49,20 @@ function App() {
       <h1>Covid 19 Tracker</h1>
     <FormControl className="app__dropdown">
 
-  <select variant="outlined" value="abc">
+  <Select variant="outlined" onChange={onCountryChange } value={country }>
+  <MenuItem value="worldWide" >worldWide</MenuItem>
 
-    <MenuItem value="worldwide">worldwide</MenuItem>
-    <MenuItem value="worldwide">dd</MenuItem>
-    <MenuItem value="worldwide">dfd</MenuItem>
-    <MenuItem value="worldwide">worldwide</MenuItem>
-    <MenuItem value="worldwide">dfd</MenuItem>
+  { 
+      countries.map((country) =>(
+        <MenuItem value={country.value}>{country.name}</MenuItem>
+        
+      ))} 
 
 
-  </select>
+  </Select>
 </FormControl>
 </div>
-
+ 
 {/*Header*/}
 {/*Title - Select Input 
 dropdown field*/}
